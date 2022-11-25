2b1cafdcc0e098eaa6d0a60056efdbc46410bdb9
NikkiIV <prince_the_ripper@abv.bg>
2022-11-25 21:29:29 +0200

    Add Dealer class

--- RentACar/RentACar/Data/DataConstants.cs
+++ RentACar/RentACar/Data/DataConstants.cs
@@ -4,5 +4,9 @@
     {
         public const int CarBrandMaxLenght = 20;
         public const int CarModelMaxLenght = 30;
+        public const int DealerNameMinLength = 2;
+        public const int DealerNameMaxLength = 30;
+        public const int DealerPhoneMinLength = 6;
+        public const int DealerPhoneMaxLength = 30;
     }
 }
---
+++ RentACar/RentACar/Data/Models/Dealer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    using static DataConstants;
+
+    public class Dealer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DealerNameMaxLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(DealerPhoneMaxLength)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public IEnumerable<Car> Cars { get; set; } = new List<Car>();
+    }
+}
