830427ff1a364755a0cbb3d34ed65d7424039bbf
NikkiIV <prince_the_ripper@abv.bg>
2022-11-27 20:58:37 +0200

    Add Class Sales

---
+++ RentACar/RentACar/Data/Models/Sales.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    public class Sales
+    {        
+        public int Id { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
