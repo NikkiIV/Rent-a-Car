3cd85a0ce195bd738cb5f58bed629d0f60fd6550
NikkiIV <prince_the_ripper@abv.bg>
2022-12-04 21:54:41 +0200

    reviews addition

---
+++ RentACar/RentACar/Data/Models/Reviews.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    public class Reviews
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
