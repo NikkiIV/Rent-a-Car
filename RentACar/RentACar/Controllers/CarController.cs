d9324da1b08b0d71549ef7893456e7a527e3496e
NikkiIV <prince_the_ripper@abv.bg>
2022-12-06 21:44:39 +0200

    Edit Functionality

--- RentACar/RentACar/Controllers/CarController.cs
+++ RentACar/RentACar/Controllers/CarController.cs
@@ -1,6 +1,33 @@
-﻿namespace RentACar.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACar.Models;
+using System.Diagnostics;
+
+namespace RentACar.Controllers
 {
     public class CarController
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public CarController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
     }
 }
