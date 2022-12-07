37a9a0f889a12e0ccf0ce2fb47a8048320777442
NikkiIV <prince_the_ripper@abv.bg>
2022-12-07 22:07:04 +0200

    Updating Classes

---
+++ RentACar/RentACar/Data/Models/Maintenance.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    public class Maintenance
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Experience { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
