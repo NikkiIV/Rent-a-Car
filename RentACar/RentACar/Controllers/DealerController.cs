f4ae4dc53fa8be0572cb1749f3202ae5c2d80236
NikkiIV <prince_the_ripper@abv.bg>
2022-12-14 22:32:43 +0200

    Dealer controller add

--- RentACar/RentACar/Controllers/DealerController.cs
+++ RentACar/RentACar/Controllers/DealerController.cs
@@ -1,6 +1,57 @@
-﻿namespace RentACar.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Data;
+
+namespace RentACar.Controllers
 {
     public class DealerController
     {
+        private readonly RentACarDbContext data;
+
+        public DealersController(RentACarDbContext _data)
+        {
+            data = _data;
+        }
+
+        [Authorize]
+        public IActionResult Become()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Become(BecomeDealerFormModel dealer)
+        {
+            var userId = User.Id();
+
+            var userIdAlreadyDealer = data
+                .Dealers
+                .Any(d => d.UserId == userId);
+
+            if (userIdAlreadyDealer)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(dealer);
+            }
+
+            var dealerData = new Dealer
+            {
+                Name = dealer.Name,
+                PhoneNumber = dealer.PhoneNumber,
+                UserId = userId
+            };
+
+            data.Dealers.Add(dealerData);
+            data.SaveChanges();
+
+            TempData[GlobalMessageKey] = "Thank you for becomming a dealer";
+
+            return RedirectToAction("All", "Cars");
+        }
     }
 }
