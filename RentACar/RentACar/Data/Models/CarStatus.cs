cf388961c8c54fe85bb0a90adc322c23058349ff
NikkiIV <prince_the_ripper@abv.bg>
2022-12-19 21:51:05 +0200

    CarStatuse Add

---
+++ RentACar/RentACar/Data/Models/CarStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    public class CarStatus
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Experience { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
