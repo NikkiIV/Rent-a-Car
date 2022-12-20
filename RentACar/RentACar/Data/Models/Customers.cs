36190d70e231407f20e92d40ee6c968e5c0cb2dd
NikkiIV <prince_the_ripper@abv.bg>
2022-12-20 22:27:18 +0200

    working on users

---
+++ RentACar/RentACar/Data/Models/Customers.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    public class Customers
+    {
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Experience { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
