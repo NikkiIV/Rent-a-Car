e502e6f3b635b6aea19975faa38cf1e460a39073
NikkiIV <prince_the_ripper@abv.bg>
2022-12-21 21:53:53 +0200

    TempData

--- RentACar/RentACar/Data/Models/Comments.cs
+++ RentACar/RentACar/Data/Models/Comments.cs
@@ -2,21 +2,13 @@
 
 namespace RentACar.Data.Models
 {
-    public class Customers
+    public class Comments
     {
+
         public int Id { get; init; }
 
         [Required]
         public string Name { get; set; }
-
-        [Required]
-        public int Age { get; set; }
-
-        [Required]
-        public string Gender { get; set; }
-        //add
-        [Required]
-        public string Experience { get; set; }
 
         [Required]
         public string Description { get; set; }
