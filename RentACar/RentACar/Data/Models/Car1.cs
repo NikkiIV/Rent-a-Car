61ef76d614d1731f7ed767e3b2fa07c2404677e7
NikkiIV <prince_the_ripper@abv.bg>
2022-12-27 23:09:31 +0200

    working on functionality

---
+++ RentACar/RentACar/Data/Models/Car1.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Models
+{
+    using static DataConstants.Car;
+
+    public class Car1
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(CarBrandMaxLenght)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(CarModelMaxLenght)]
+        public string Model { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public int Year { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; init; }
+
+        public int DealerId { get; set; }
+
+        public Dealer Dealer { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+    }
+}
